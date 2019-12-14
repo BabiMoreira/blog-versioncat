3deaf4cf2889210f500d91d7275b0bcc073af16e
Bárbara Moreira <babisantos631@gmail.com>
2019-12-14 20:04:11 -0300

    fazer campos de cadastro e a o get nas views

--- website/views.py
+++ website/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from website.models import Aluno
 
 def home(request):
     return render(request, 'home.html')
 
 def cadastro(request):
+    contexto ={}
+    if request.method == 'POST':
+        aluno = Aluno()
+        aluno.nome= request.POST.get('nome')
+        aluno.email= request.POST.get('email')
+        aluno.senha= request.POST.get('senha')
+        aluno.frase= request.POST.get('frase')
+        aluno.save()
+        return render(request, 'home.html')
     return render(request, 'cadastro.html')
