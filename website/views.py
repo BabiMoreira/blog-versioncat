397f5c113097ec1a9ee81af99697ee661749224b
Bárbara Moreira <babisantos631@gmail.com>
2019-12-15 12:15:42 -0300

    tentar fazer login, porém cadastro nao esta funcionando

--- website/views.py
+++ website/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-from website.models import Aluno
+from website.models import Usuario, Aluno
 
 def home(request):
     return render(request, 'home.html')
 
 def cadastro(request):
-    contexto ={}
     if request.method == 'POST':
         aluno = Aluno()
         aluno.nome= request.POST.get('nome')
@@ -13,5 +12,7 @@ def cadastro(request):
         aluno.senha= request.POST.get('senha')
         aluno.frase= request.POST.get('frase')
         aluno.save()
-        return render(request, 'home.html')
     return render(request, 'cadastro.html')
+
+def login(request):
+    return render(request, 'login.html')
